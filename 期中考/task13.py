92f0fec957866abaad2967975d92320450bc3a57
C110156248 <102135389+C110156248@users.noreply.github.com>
2022-04-24 12:30:25 +0800

    Delete 期中考 directory

--- 期中考/task13.py
+++
@@ -1,6 +0,0 @@
-# a=input("輸入一字串")
-a="12345"
-if a == a[::-1]:
-    print("yes")
-else:
-    print("no")
--- 期中考/task9.py
+++
@@ -1,6 +0,0 @@
-a=input("請輸入兩個字串用逗點隔開")
-b=a.split(",")
-if b[0] in b[1]:
-    print("yes")
-else:
-    print("no")
